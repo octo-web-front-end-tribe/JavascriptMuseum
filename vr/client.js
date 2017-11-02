95afbe41863ca2f15e32797db2e62463caad2dfa
Anass Oumiri <aoumiri@octo.com>
2017-11-02 22:24:11 +0100

    Enabling Hot reload module

--- vr/client.js
+++ vr/client.js
@@ -7,8 +7,8 @@ import {VRInstance} from 'react-vr-web';
 
 function init(bundle, parent, options) {
   const vr = new VRInstance(bundle, 'JavascriptMuseum', parent, {
-    // Add custom options here
     ...options,
+		enableHotReload: true,
   });
   vr.render = function() {
     // Any custom behavior you want to perform on each frame goes here
